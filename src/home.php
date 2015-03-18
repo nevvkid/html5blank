9e0322d8339f9c33d81be4703fb9542d2ae4c445
nevvkid <johannes.lohbihler@gmail.com>
2015-03-18 18:19:32 +0100

    working on blog dom

--- src/home.php
+++ src/home.php
@@ -15,8 +15,6 @@
         <!-- section -->
         <section>
 
-          <h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
-
           <?php get_template_part('loop'); ?>
 
           <?php get_template_part('pagination'); ?>
--- src/single.php
+++ src/single.php
@@ -1,75 +1,37 @@
-<?php include (TEMPLATEPATH . '/header.php'); ?>
+    <?php include (TEMPLATEPATH . '/header.php'); ?>
 
-<main class="cd-main-content">
-    <div class="cd-scrolling-bg cd-color-4">
-      <div class="cd-container blog-post">
+    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-	<!-- section -->
-	<section>
+    <main class="cd-main-content" role="main">
+      <div class="cd-scrolling-bg cd-color-4">
+        <div class="cd-container blog-post">
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> class="post">
+            <div class="post-meta">
+              <h1 class="post-title">
+                <a href="<?php the_permalink(); ?>" itemprop="url" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+              </h1>
+              <div class="cf post-meta-text">
+                <time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>"><?php the_date(); ?> <?php the_time(); ?></time>
+              </div>
+            </div>
+            <section class="post-content">
+              <?php the_content(); // Dynamic Content ?>
+            </section>
+          </article>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <?php endwhile; ?>
+          <?php else: ?>
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+          <!-- article -->
+          <article>
+            <h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
+          </article>
+          <!-- /article -->
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+          <?php endif; ?>
 
-			<!-- post details -->
-			<span class="date">
-				<time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>">
-					<?php the_date(); ?> <?php the_time(); ?>
-				</time>
-			</span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
-
-			<?php the_content(); // Dynamic Content ?>
-
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
-
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
-
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
-
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
-
-			<?php comments_template(); ?>
-
-		</article>
-		<!-- /article -->
-
-	<?php endwhile; ?>
-
-	<?php else: ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-
+        </div>
       </div>
-    </div>
-  </main>
-
-<?php include (TEMPLATEPATH . '/page-landing-footer.php'); ?>
+    </main>
+    <?php include (TEMPLATEPATH . '/page-landing-footer.php'); ?>
