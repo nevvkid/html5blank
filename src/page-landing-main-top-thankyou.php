d0ee38e2cd91106b0e586d526b717cfb8b44203d
nevvkid <johannes.lohbihler@gmail.com>
2015-03-19 20:20:14 +0100

    added image field in thank you page of referraltool

--- src/page-landing-main-top-thankyou.php
+++ src/page-landing-main-top-thankyou.php
@@ -2,5 +2,15 @@
     <div class="cd-scrolling-bg cd-color-1">
       <div class="cd-container thankyou">
 
+      <?php
+
+      $image = get_field('referral_-_thank_you_-_image');
+
+      if( !empty($image) ): ?>
+
+      <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+
+      <?php endif; ?>
+
       <h1><?php the_field('referral_-_thank_you_-_headline'); ?></h1>
       <p><?php the_field('referral_-_thank_you_-_copytext'); ?></p>
