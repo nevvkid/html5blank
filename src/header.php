9d49a42b778333c68fa0da0e80b8d57a6d5c8670
nevvkid <johannes.lohbihler@gmail.com>
2015-03-20 17:09:09 +0100

    twitter meta tag debugging

--- src/header.php
+++ src/header.php
@@ -11,13 +11,6 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- facebook -->
-    <meta property="og:title" content="<?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' : '; } ?>"/>
-    <meta name="og:type" content="product">
-    <meta property="og:description" content="Can´t wait for @Zedfy_Germany to launch. - smart moving #LED #lights!"/>
-    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/page-landing/rendering2.jpg"/>
-    <meta property="og:url" content="<?php echo esc_url( home_url( '/' ) ); ?>"/>
-
     <!-- twitter card -->
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@zedfy_germany" />
@@ -25,6 +18,13 @@
     <meta name="twitter:description" content="Can´t wait for @Zedfy_Germany to launch.">
     <meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/img/page-landing/rendering2.jpg" />
     <meta name="twitter:url" content="<?php echo esc_url( home_url( '/' ) ); ?>" />
+
+    <!-- facebook -->
+    <meta property="og:title" content="<?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' : '; } ?>"/>
+    <meta name="og:type" content="product">
+    <meta property="og:description" content="Can´t wait for @Zedfy_Germany to launch. Check out the early bird discounts #zedcon - smart moving #LED #lights!"/>
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/page-landing/rendering2.jpg"/>
+    <meta property="og:url" content="<?php echo esc_url( home_url( '/' ) ); ?>"/>
 
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,700|Merriweather:400,700' rel='stylesheet' type='text/css'>
 
