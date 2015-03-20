ff94211bae5495574f4bc80382431b5a2d9a2441
nevvkid <johannes.lohbihler@gmail.com>
2015-03-20 17:14:39 +0100

    google verification

--- src/header.php
+++ src/header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="google-site-verification" content="dBnA6p6voa7-_vAHtikgOfqEkEjpQO0LJhnXsmsNYkU" />
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' : '; } ?><?php bloginfo('name'); ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
