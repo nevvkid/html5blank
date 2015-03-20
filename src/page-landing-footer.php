cc627850000cb6149adb0db69e2a12c1b2fc93de
nevvkid <johannes.lohbihler@gmail.com>
2015-03-20 17:20:35 +0100

    google analytics

--- src/page-landing-footer.php
+++ src/page-landing-footer.php
@@ -2,13 +2,13 @@
 
     <!-- analytics -->
     <script>
-    (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-    (f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-    l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-    ga('send', 'pageview');
+      (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+      e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+      e.src='//www.google-analytics.com/analytics.js';
+      r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+      ga('create','UA-36949228-2','auto');
+      ga('send','pageview');
     </script>
-
   </body>
 </html>
