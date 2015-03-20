c341667b06bdcf23023473a443f5f18b2f072f54
nevvkid <johannes.lohbihler@gmail.com>
2015-03-20 16:38:56 +0100

    twitter meta tag debugging

--- src/header.php
+++ src/header.php
@@ -10,7 +10,6 @@
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="Can´t wait for @Zedfy_Germany to launch. Check out the early bird discounts #zedcon - smart moving #LED #lights!">
 
     <!-- facebook -->
     <meta property="og:title" content="<?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' : '; } ?>"/>
