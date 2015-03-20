21d4f2745bb75a8f134bb6fb29699c1fc6fc180c
nevvkid <johannes.lohbihler@gmail.com>
2015-03-20 15:30:57 +0100

    twitter text changed

--- src/header.php
+++ src/header.php
@@ -10,7 +10,7 @@
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="Shaving is evolving. Didn’t want to leave you all behind. Check out Harry’s here.">
+		<meta name="description" content="Can´t wait for @Zedfy_Germany to launch. Check out the early bird discounts #zedcon - smart moving #LED #lights!">
 
     <!-- facebook -->
     <meta property="og:title" content="<?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' : '; } ?>"/>
