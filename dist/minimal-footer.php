0c18fe11bc289036563c1bcd7eb17affe7ebad9f
nevvkid <johannes.lohbihler@gmail.com>
2015-03-23 00:29:36 +0100

    remove custom post type in functions

--- dist/minimal-footer.php
+++
@@ -1,3 +0,0 @@
-      <div class="cd-footer-inner">
-        <p>&copy; <?php echo date('Y'); ?> <strong><?php bloginfo('name'); ?></strong> &ndash; <?php icl_link_to_element(81); ?></p>
-      </div>
--- dist/page-landing-main-bottom-thankyou.php
+++
@@ -1,7 +0,0 @@
-      </div> <!-- cd-container -->
-    </div> <!-- cd-scrolling-bg -->
-    <div class="cd-footer">
-      <?php include (TEMPLATEPATH . '/minimal-footer.php'); ?>
-    </div>
-
-  </main> <!-- cd-main-content -->
