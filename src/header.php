8c117073d777c7ac58cc0e53a8acf38f15644544
nevvkid <johannes.lohbihler@gmail.com>
2015-03-23 19:34:04 +0100

    favicon

--- src/header.php
+++ src/header.php
@@ -6,8 +6,8 @@
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' : '; } ?><?php bloginfo('name'); ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
-    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
-    <link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
+    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon.png">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
