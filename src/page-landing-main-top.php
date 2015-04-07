2c73f62d6a8a6d36cc6c7f198fb49e29001f123d
nevvkid <johannes.lohbihler@gmail.com>
2015-04-07 19:50:24 +0200

    indiegogo button

--- src/page-landing-main-top.php
+++ src/page-landing-main-top.php
@@ -12,5 +12,6 @@
             <?php endif; ?>
             <?php the_field('intro_text'); ?></h1>
       </div>
+      <div class="cd-indiegogo"><a href="http://bit.ly/1H2jxGj"><img src="<?php echo get_template_directory_uri(); ?>/img/indiegogo-big-button.png" alt="indiegogo button" class="indiegogo-button"></a></div>
       <div class="cd-referral">
       <h3><?php the_field('newsletter_-__call_to_action'); ?></h3>
